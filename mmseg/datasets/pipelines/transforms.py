134196dd1220652e2bc18047b300d89e9a9a8982
Fabian Schuetze <fabian.schutze@eui.eu>
2022-07-10 12:35:13 +0200

    added more augmentations

--- mmseg/datasets/pipelines/transforms.py
+++ mmseg/datasets/pipelines/transforms.py
@@ -5,8 +5,83 @@ import mmcv
 import numpy as np
 from mmcv.utils import deprecated_api_warning, is_tuple_of
 from numpy import random
+import albumentations as A
+import cv2
 
 from ..builder import PIPELINES
+
+class Unsharpen(A.ImageOnlyTransform):
+    def __init__(self, k_limits=(3, 7), strength_limits=(0., 2.), p=0.5):
+        super().__init__()
+        self.k_limits = k_limits
+        self.strength_limits = strength_limits
+        self.p = p
+
+    def apply(self, img, **params):
+        if np.random.rand() > self.p:
+            return img
+        k = np.random.randint(self.k_limits[0] // 2, self.k_limits[1] // 2 + 1) * 2 + 1
+        s = k / 3
+        blur = cv2.GaussianBlur(img, (k, k), s)
+        strength = np.random.uniform(*self.strength_limits)
+        unsharpened = cv2.addWeighted(img, 1 + strength, blur, -strength, 0)
+        return unsharpened
+
+class DebayerArtefacts(A.ImageOnlyTransform):
+    def __init__(self, p=0.5):
+        super().__init__()
+        self.p = p
+
+    def apply(self, img, **params):
+        if np.random.rand() > self.p:
+            return img
+        assert img.dtype == np.uint8
+        # permute channels before bayering/debayering to cover different bayer formats
+        channel_idxs = np.random.permutation(3)
+        channel_idxs_inv = np.empty(3, dtype=int)
+        channel_idxs_inv[channel_idxs] = 0, 1, 2
+
+        # assemble bayer image
+        bayer = np.zeros(img.shape[:2], dtype=img.dtype)
+        bayer[::2, ::2] = img[::2, ::2, channel_idxs[2]]
+        bayer[1::2, ::2] = img[1::2, ::2, channel_idxs[1]]
+        bayer[::2, 1::2] = img[::2, 1::2, channel_idxs[1]]
+        bayer[1::2, 1::2] = img[1::2, 1::2, channel_idxs[0]]
+
+        # debayer
+        debayer_method = np.random.choice((cv2.COLOR_BAYER_BG2BGR, cv2.COLOR_BAYER_BG2BGR_EA))
+        debayered = cv2.cvtColor(bayer, debayer_method)[..., channel_idxs_inv]
+        return debayered
+
+
+
+@PIPELINES.register_module()
+class MyAugmentations(object):
+    """Resize images & seg to multiple of divisor.
+
+    Args:
+        size_divisor (int): images and gt seg maps need to resize to multiple
+            of size_divisor. Default: 32.
+        interpolation (str, optional): The interpolation mode of image resize.
+            Default: None
+    """
+
+    def __init__(self):
+        self.tfms=A.Compose([
+            A.GaussianBlur(blur_limit=(1, 3)),
+            A.ISONoise(),
+            A.GaussNoise(),
+            Unsharpen(),
+            DebayerArtefacts(),
+            A.CLAHE(),  # could probably be moved to the post-crop augmentations
+            A.GaussianBlur(blur_limit=(1, 3)),
+        ])
+
+    def __call__(self, results):
+        img = results['img']
+        img = self.tfms(image=img)['image']
+        results['img'] = img
+        return results
 
 
 @PIPELINES.register_module()
